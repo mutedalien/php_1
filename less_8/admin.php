72a7a9a075b630e1ff225397f88f3d92fc141679
mutedalien <chel_c@mail.ru>
2020-02-12 00:04:18 +0300

    Дополнил механизмом добавления товара в админке

--- less_8/admin.php
+++ less_8/admin.php
@@ -20,6 +20,85 @@ $ordersQuery = mysqli_query($link, $query);
 <body>
 <h2>Админка</h2>
 <a href="index.php">Главная</a>
+
+<br>
+
+<?php
+  $sql = mysqli_query($link, 'SELECT `ID`, `good`, `price` FROM `goods`');
+  while ($result = mysqli_fetch_array($sql)) {
+    echo "{$result['good']}: {$result['price']} рублей<br>";
+  }
+?>
+
+<!--    -->
+
+<?php
+//Если переменная Name передана
+    if (isset($_POST["good"])) {
+      //Если это запрос на обновление, то обновляем
+      if (isset($_GET['red'])) {
+        $sql = mysqli_query($link, "UPDATE `goods` SET `good` = '{$_POST['good']}',`price` = '{$_POST['price']}',`description` = '{$_POST['description']}', `good_img` = '{$_POST['good_img']}' WHERE `ID`={$_GET['red']}");
+      } else {
+        //Иначе вставляем данные, подставляя их в запрос
+        $sql = mysqli_query($link, "INSERT INTO `goods` (`good`, `price`, `description`, `good_img`) VALUES ('{$_POST['good']}', '{$_POST['price']}', '{$_POST['description']}', '{$_POST['good_img']}')");
+      }
+
+      //Если вставка прошла успешно
+      if ($sql) {
+        echo '<p>Успешно!</p>';
+      } else {
+        echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
+      }
+    }
+
+    //Удаляем, если что
+    if (isset($_GET['del'])) {
+      $sql = mysqli_query($link, "DELETE FROM `goods` WHERE `ID` = {$_GET['del']}");
+      if ($sql) {
+        echo "<p>Товар удален.</p>";
+      } else {
+        echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
+      }
+    }
+
+    //Если передана переменная red, то надо обновлять данные. Для начала достанем их из БД
+    if (isset($_GET['red'])) {
+      $sql = mysqli_query($link, "SELECT `ID`, `good`, `price`, `description`, `good_img` FROM `goods` WHERE `ID`={$_GET['red']}");
+      $product = mysqli_fetch_array($sql);
+    }
+  ?>
+
+  <form action="" method="post">
+    <table>
+      <tr>
+        <td>Наименование:</td>
+        <td><input type="text" name="good" value="<?= isset($_GET['red']) ? $product['good'] : ''; ?>"></td>
+      </tr>
+      <tr>
+        <td>Цена:</td>
+        <td><input type="text" name="price" size="3" value="<?= isset($_GET['red']) ? $product['price'] : ''; ?>"> руб.</td>
+      </tr>
+      <tr>
+        <td>Описание:</td>
+        <td><input type="text" name="description" value="<?= isset($_GET['red']) ? $product['description'] : ''; ?>"></td>
+      </tr>
+      <td>Путь к картинке:</td>
+        <td><input type="text" name="good_img" value="<?= isset($_GET['red']) ? $product['good_img'] : ''; ?>"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><input type="submit" value="OK"></td>
+      </tr>
+    </table>
+  </form>
+  <?php
+  //Получаем данные
+  $sql = mysqli_query($link, 'SELECT `ID`, `good`, `price`, `description`, `good_img` FROM `goods`');
+  while ($result = mysqli_fetch_array($sql)) {
+    echo "<p>{$result['ID']}) {$result['good']} - {$result['price']} ₽ - <a href='?del={$result['ID']}'>Удалить</a> - <a href='?red={$result['ID']}'>Редактировать</a></p>";
+  }
+  ?>
+
+
 <?php
 if (!$_SESSION['is_admin']): // Проверка на админа
   echo 'Доступ разрешен только администраторам!';
