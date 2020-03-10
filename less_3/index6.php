df444a937998e88d7f66e0cec76a499e8d12cdd0
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-03-10 21:14:48 +0300

    Update index6.php

--- less_3/index6.php
+++ less_3/index6.php
@@ -3,7 +3,7 @@
 $menu = array ("Главная", "Товары" => array ("Смартфоны", "Планшеты", "Ноутбуки"), "О нас" => array ("Команда", "В прессе", "Отзывы"), "Обратная связь");
 echo "<ul>";
 foreach ($menu as $more => $p) {
-if (gettype($p) == "array" ) {
+if (gettype($p) == "array" ) { //gettype - получает тип переменной 
 		echo "<li>".$more."<ul>";
 	foreach ($p as $t) {
 		echo "<li>".$t."</li>";
